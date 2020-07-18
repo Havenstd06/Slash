760f9c37330087aa6045cd93f580f7d97ced6b5c
Havens <me@hvs.cx>
2020-07-18 17:32:48 +0200

    Add TailwindUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,19 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: [
     './resources/views/**/*.blade.php',
     './resources/css/**/*.css',
   ],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    }
   },
   variants: {},
   plugins: [
-    require('@tailwindcss/custom-forms')
+    require('@tailwindcss/ui')
   ]
 }
