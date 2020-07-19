a6c69f56c5ec0744bfe635c9271120d09351a004
Havens <me@hvs.cx>
2020-07-19 19:17:31 +0200

    Change navbar js name

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,19 +1,19 @@
-<nav x-data="{ open: false }" class="mb-4 bg-white shadow">
+<nav x-data="{ drop: false }" class="z-50 mb-4 bg-white shadow">
     <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
     <div class="flex justify-between h-16">
         <div class="flex">
         @auth
         <div class="flex items-center mr-2 -ml-2 md:hidden">
             <!-- Mobile menu button -->
-            <button @click="open = !open" class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500" x-bind:aria-label="open ? 'Close main menu' : 'Main menu'" aria-label="Main menu" x-bind:aria-expanded="open">
-            <!-- Icon when menu is closed. -->
-            <svg x-state:on="Menu open" x-state:off="Menu closed" :class="{ 'hidden': open, 'block': !open }" class="block w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
-            </svg>
-            <!-- Icon when menu is open. -->
-            <svg x-state:on="Menu open" x-state:off="Menu closed" :class="{ 'hidden': !open, 'block': open }" class="hidden w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-            </svg>
+            <button @click="drop = !drop" class="inline-flex items-center justify-center p-2 text-gray-400 transition duration-150 ease-in-out rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500" x-bind:aria-label="drop ? 'Close main menu' : 'Main menu'" aria-label="Main menu" x-bind:aria-expanded="drop">
+              <!-- Icon when menu is closed. -->
+              <svg x-state:on="Menu open" x-state:off="Menu closed" :class="{ 'hidden': drop, 'block': !drop }" class="block w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+              </svg>
+              <!-- Icon when menu is open. -->
+              <svg x-state:on="Menu open" x-state:off="Menu closed" :class="{ 'hidden': !drop, 'block': drop }" class="hidden w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+              </svg>
             </button>
         </div>
         @endauth
@@ -27,14 +27,14 @@
         @auth
           <div class="hidden md:ml-4 md:flex-shrink-0 md:flex md:items-center">
             <!-- Profile dropdown -->
-            <div @click.away="open = false" class="relative ml-3" x-data="{ open: false }">
+            <div @click.away="drop = false" class="relative ml-3" x-data="{ drop: false }">
               <div class="flex items-center">
-                  <h2 class="mr-2 text-lg font-medium text-gray-700">{{ auth()->user()->name }}</h2>
-                  <button @click="open = !open" class="flex text-sm transition duration-150 ease-in-out border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300" id="user-menu" aria-label="User menu" aria-haspopup="true" x-bind:aria-expanded="open">
+                  <h2 class="mr-2 text-lg font-medium text-gray-700 truncate">{{ auth()->user()->name }}</h2>
+                  <button @click="drop = !drop" class="flex text-sm transition duration-150 ease-in-out border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300" id="user-menu" aria-label="User menu" aria-haspopup="true" x-bind:aria-expanded="drop">
                   <img class="w-10 h-10 rounded-full" src="{{ url('img/profile.png') }}" alt="">
                   </button>
               </div>
-              <div x-show="open" x-description="Profile dropdown panel, show/hide based on dropdown state." x-transition:enter="transition ease-out duration-200" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute right-0 w-48 mt-2 origin-top-right rounded-md shadow-lg" style="display: none;">
+              <div x-show="drop" x-description="Profile dropdown panel, show/hide based on dropdown state." x-transition:enter="transition ease-out duration-200" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute right-0 w-48 mt-2 origin-top-right rounded-md shadow-lg" style="display: none;">
                   <div class="py-1 bg-white rounded-md shadow-xs" role="menu" aria-orientation="vertical" aria-labelledby="user-menu">
                   {{-- <a href="#" class="block px-4 py-2 text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:bg-gray-100" role="menuitem">My links</a> --}}
                   <a href="{{ route('logout') }}" class="block px-4 py-2 text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus:bg-gray-100" role="menuitem" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Sign out</a>
@@ -60,7 +60,7 @@
     </div>
 
     @auth
-    <div x-description="Mobile menu, toggle classes based on menu state." x-state:on="Menu open" x-state:off="Menu closed" :class="{ 'block': open, 'hidden': !open }" class="hidden md:hidden">
+    <div x-description="Mobile menu, toggle classes based on menu state." x-state:on="Menu open" x-state:off="Menu closed" :class="{ 'block': drop, 'hidden': !drop }" class="hidden md:hidden">
       <div class="pt-4 pb-3 border-t border-gray-200">
           <div class="flex items-center px-4 sm:px-6">
             <div class="flex-shrink-0">
